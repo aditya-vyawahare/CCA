ea451fa13aa366f3709c9d8f62c84f4bedf7ddd4
aditya-vyawahare <157919971+aditya-vyawahare@users.noreply.github.com>
2024-01-27 16:28:15 -0600

    Update serve.py

--- serve.py
+++ serve.py
@@ -8,15 +8,15 @@ def run_script():
     try:
         Popen(['python3','stress_cpu.py'])
     except:
-        print('Error coudnt run stress.py')
+        print('Error coudnt run stress_cpu.py')
 
 @app.route('/',methods=['GET','POST'])
 def user():
     if request.method=='POST':
         run_script()
-        return jsonify({'message': 'Running Script stress.py' })
+        return jsonify({'message': 'Running Script stress_cpu.py' })
     
     if request.method=='GET':
-        return jsonify({'IP': socket.gethostbyname(socket.gethostname())})
+        return str(socket.gethostbyname(socket.gethostname()))
     
 app.run(host='0.0.0.0', port=5000)
